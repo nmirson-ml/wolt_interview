76586c98e4f8e820d7123b3eef7c7ab18f8b6804
Nicolas Mirson <nicolas.mirson@gmail.com>
2024-12-06 12:01:37 +0100

    database_init

---
+++ database_init.py
@@ -0,0 +1,12 @@
+import os
+import duckdb
+# Path to the datamart.duckdb file
+db_path = '../shared/db/datamart.duckdb'
+# Check if the database file exists
+if os.path.exists(db_path):
+    # Delete the existing database file
+    os.remove(db_path)
+    print(f"Deleted existing database at {db_path}")
+# Create a new database connection with the same file name
+conn = duckdb.connect(db_path)
+print(f"Created a new database at {db_path}")
